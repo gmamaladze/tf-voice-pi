1943a78f2bd8dc09d74ccd64186935fb065bd481
George Mamaladze <gmamaladze@gmail.com>
2017-12-06 22:28:10 +0100

    Discovered that device only supports certain rates. Converting

--- mic_pi.py
+++ mic_pi.py
@@ -3,13 +3,14 @@ import time
 import numpy as np
 import struct
 import mic_device
+import audioop
 
 FORMAT = alsaaudio.PCM_FORMAT_S16_LE
 CHANNELS = 1
 RATE = 16000
 MAX_INT16 = np.iinfo(np.int16).max
 CHUNK_SIZE = 1000
-DEFAULT_DEVICE = "sysdefault:CARD=Device" #None
+DEFAULT_DEVICE = "hw:CARD=Device,DEV=0" #None
 
 end = False
 
@@ -21,16 +22,19 @@ def get_mic_data(chunk_size=CHUNK_SIZE, device=DEFAULT_DEVICE):
     inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, device)
 
     chunk_byte_count = chunk_size * 2
-    inp.setperiodsize(CHUNK_SIZE)
-    inp.setchannels(CHANNELS)
-    inp.setrate(RATE)
-    inp.setformat(FORMAT)
+    inp.setchannels(1)
+    inp.setrate(44100)
+    inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+    inp.setperiodsize(160)
+
+    state = None
 
     data_buffer = ""
     while not end:
         l, data = inp.read()
-        if l:
-            data_buffer += data
+        if l>0:
+            data1, state = audioop.ratecv(data, 2, 1, 44100, 16000, state)
+            data_buffer += data1
             while len(data_buffer) >= chunk_byte_count:
                 chunk = data_buffer[:chunk_byte_count]
                 data_int = struct.unpack('<'+'h'*chunk_size, chunk)
@@ -38,4 +42,5 @@ def get_mic_data(chunk_size=CHUNK_SIZE, device=DEFAULT_DEVICE):
                 yield data_float
                 data_buffer = data_buffer[chunk_byte_count:]
         else:
-            time.sleep(.001)
+            pass
+            #time.sleep(.001)
